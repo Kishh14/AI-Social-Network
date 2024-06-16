97d470507f6c0316a9b019e48e7ea3c5815080b7
Kishh14 <salvikishan833@gmai.com>
2024-06-16 22:07:57 +0530

    Analytics Configured

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,6 +8,15 @@ import { Provider } from "react-redux";
 import { persistor, store } from "./Redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-3ZL3KFMBM2");
+
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
